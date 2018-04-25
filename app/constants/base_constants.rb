a67ba84e25d43d767eaa47051fa27633c33012bc
Anton Kiba <akiba788@mail.ru>
2018-04-25 21:55:06 +0300

    update home page

---
+++ app/constants/base_constants.rb
@@ -0,0 +1,8 @@
+class BaseConstants
+  class << self
+    # a b c d e f g h i j k l m n o p q r s t u v w x y z
+    def available_currencies_codes
+      %w[R01235]
+    end
+  end
+end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,12 @@
 class HomeController < ApplicationController
+  helper_method :currencies_collection
+
   def index
+  end
+
+protected
+
+  def currencies_collection
+    @currencies_collection ||= Currency.where(parent_code: BaseConstants.available_currencies_codes)
   end
 end
